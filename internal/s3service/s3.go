98dbdde2609412bc7eb5ace21e137c6fb4e66c4b
felixsolom <felix.solom@gmail.com>
2025-08-17 21:01:18 +0300

    aws-#3

--- internal/s3service/s3.go
+++ internal/s3service/s3.go
@@ -1,9 +1,11 @@
 package s3service
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	awsconfig "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -30,4 +32,16 @@ func New(cfg config.AWSConfig) (*Service, error) {
 		S3Client:   s3Client,
 		BucketName: cfg.BucketName,
 	}, nil
+}
+
+func (s *Service) UploadFile(ctx context.Context, key string, data []byte) error {
+	_, err := s.S3Client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket: aws.String(s.BucketName),
+		Key:    aws.String(key),
+		Body:   bytes.NewReader(data),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to upload file to AWS: %w", err)
+	}
+	return nil
 }
